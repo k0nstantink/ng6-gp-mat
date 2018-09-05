b10675f8f4cc1e08f2720b5cbc97eb588549e69e
k0nstantink <konstantin.konstantinovic@gmail.com>
2018-09-05 16:15:02 +0100

    dist

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-
 app.use(express.static('./dist/ng6-gp-app'));
+
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join('./dist/ng6-gp-app/index.html'));
+res.sendFile(path.join(__dirname,'/dist/ng6-gp-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 8080);
